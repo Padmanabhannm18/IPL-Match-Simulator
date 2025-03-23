e40417da897fb873e498addb5bb8e5535612c6c2
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-23 22:26:18 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import streamlit as st
 def load_data():
-    matches = pd.read_csv("data/IPL Matches 2008-2020.csv")
-    deliveries = pd.read_csv("data/IPL Ball-by-Ball 2008-2020.csv")
+    matches = pd.read_csv("data\matches.csv")
+    deliveries = pd.read_csv("data\deliveries.csv")
     
-    deliveries = deliveries.sort_values(by=["id", "inning", "over", "ball"]).reset_index(drop=True)
+    deliveries = deliveries.sort_values(by=["match_id", "inning", "over", "ball"]).reset_index(drop=True)
 
     # Extract year from the 'date' column to create a 'season' column
     matches["date"] = pd.to_datetime(matches["date"])  # Convert to datetime
